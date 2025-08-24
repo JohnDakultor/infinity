328cd16ccb42d62eb58dd4fb79e1513db60ad852
Germel Marata <germeljohnmarata@gmail.com>
2025-08-24 11:16:08 +0800

    modified seed email and password

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -6,8 +6,8 @@ import { PrismaClient } from "@/generated/prisma";
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const email = "infinity@gmail.com";
-  const password = "admin123";
+  const email = "infinityFitness@gmail.com";
+  const password = "adminhddgrz478rz478a8cn";
 
   const existing = await prisma.user.findUnique({ where: { email } });
 
